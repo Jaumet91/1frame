00f130e8a6a597071d0c6b85edff03bdeb16888a
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-22 16:17:56 +0200

    Sidebar.tsx: default render test passed

--- src/features/Sidebar/Sidebar.tsx
+++ src/features/Sidebar/Sidebar.tsx
@@ -1,6 +1,82 @@
-
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar visible'>
+
+      <div className='sidebar__top'>
+        <a className='sidebar__logo' href='index'>
+          <img 
+          aria-label='logo-img'
+          className='sidebar__pic sidebar__pic_white' 
+          src='/assets/img/logo.svg' 
+          alt='light-logo'/>
+          <img 
+          aria-label='dark_logo-img'
+          className='sidebar__pic sidebar__pic_black' 
+          src='/assets/img/logo-white.svg' 
+          alt='dark-logo'/>
+        </a>
+        <button aria-label='burguer-btn' className='sidebar__burguer'></button>
+        <button aria-label='open_close-btn' className='sidebar__close'>
+          <svg className='icon icon-close'>
+            <use xlinkHref='img/sprite.svg#icon-close'></use>
+          </svg>
+        </button>
+      </div>
+
+      <div className='sidebar__wrapper'>
+        <div className='sidebar__inner'>
+          <div className='sidebar__group'>
+            <div className='sidebar__menu'>
+              <ul>
+                <li>News Feed</li>
+                <li>Trending</li>
+                <li>Platforms</li>
+                <li>Genres</li>
+              </ul>
+            </div>
+          </div>
+          <div className='sidebar__group'>
+            <div className='sidebar__caption caption-sm'>
+              <span>1Frame Gaming</span>
+            </div>
+            <div className='sidebar__menu'>
+              <ul>
+                <li>Settings</li>
+                <li>Analytics</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='sidebar__banner'>
+          <div className='sidebar__bg'></div>
+          <div className='sidebar__details'>
+            <div className='sidebar_title h5'> Say Hello to the creator's page</div>
+            <button 
+              aria-label='contact-tbn' 
+              className='sidebar__btn btn btn_purple btn_wide'>
+                Hello!
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className='sidebar__bottom'>
+        <label className='switch switch_theme js-switch-theme'>
+          <input className='switch__input' type='checkbox' />
+          <span className='switch__in'>
+            <span className='switch__box'></span>
+            <span className='switch__icon'>
+              <svg className='icon icon-moon'>
+                <use xlinkHref='img/sprite.svg#icon-moon'></use>
+              </svg>
+              <svg className='icon icon-sun'>
+                <use xlinkHref='img/sprite.svg#icon-sun'></use>
+              </svg>
+            </span>
+          </span>
+        </label>
+      </div>
+
+    </div>
   )
 }
--- tests/features/Sidebar/Sidebar.test.tsx
+++ tests/features/Sidebar/Sidebar.test.tsx
@@ -6,14 +6,26 @@ import { Sidebar } from '../../../src/features/Sidebar/Sidebar';
 describe('Tests in Navbar.tsx', () => { 
 
   test('should render the navigation menu by default', () => { 
-    const {container} = render (<Sidebar />);
+    render (<Sidebar />);
+
+    const logoImg: HTMLImageElement = screen.getByRole('img', {name:'logo-img'});
+    expect(logoImg.src).toContain('/assets/img/logo.svg');
+
+    const darkLogoImg: HTMLImageElement = screen.getByRole('img', {name:'dark_logo-img'});
+    expect(darkLogoImg.src).toContain('/assets/img/logo-white.svg');
 
     expect(screen.getAllByText('News Feed'));
     expect(screen.getAllByText('Trending'));
     expect(screen.getAllByText('Platforms'));
     expect(screen.getAllByText('Genres'));
+    expect(screen.getAllByText('1Frame Gaming'));
     expect(screen.getAllByText('Settings'));
-    expect(screen.getAllByText('Analitics'));
+    expect(screen.getAllByText('Analytics'));
+    expect(screen.getAllByText("Say Hello to the creator's page"));
+
+    const burguerBtn = screen.getByLabelText('burguer-btn');
+    const openCloseBtn = screen.getByLabelText('open_close-btn');
+    const contactBtn = screen.getByLabelText('contact-tbn');
 
    })
 
